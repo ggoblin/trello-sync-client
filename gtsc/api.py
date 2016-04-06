962e4277f493b87fc30e792df039919c6caf160e
eternnoir <eternnoir@gmail.com>
2016-04-06 15:09:03 +0800

    Get members done.

--- gtsc/api.py
+++ gtsc/api.py
@@ -18,6 +18,17 @@ def get_boards():
     return reslut.json()
 
 
+def get_all_member(board_name):
+    method_name = 'boards/%s/members' % gtsc.BOARD_ID
+    api_url = get_full_api_url(method_name)
+    print(api_url)
+    reslut = requests.get(api_url)
+    if reslut.status_code != 200:
+        gtsc.d.msgbox("Api error %s" % reslut.text)
+        gtsc.main()
+    return reslut.json()
+
+
 def get_full_api_url(method):
     url = TRELLO_API_URL + method + '?key=%s&token=%s' % (APP_KEY, API_TOKEN)
     return url
--- gtsc/members.py
+++ gtsc/members.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 import gtsc
-import trello
-import io
+from gtsc import api
+
 
 def members_menu():
     load_members()
 
+
 def load_members():
     gtsc.d.gauge_start(text="Get members from trello")
-    gtsc.d.gauge_update(10)  # 10% of the whole task is done
+    gtsc.d.gauge_update(50, text="Create member list.")  # 10% of the whole task is done
+    choise_list = get_member_list()
     gtsc.d.gauge_update(100, "any text here")  # work is done
     exit_code = gtsc.d.gauge_stop()  # cleanup actions
+    code, tags = gtsc.d.checklist("Members you want to import to Goblin.", choices=choise_list)
+    if code == gtsc.d.OK:
+        print("")
+
+
+def get_member_list():
+    members = api.get_all_member(gtsc.BOARD_ID)
+    print(members)
+    ret = []
+    for m in members:
+        ret.append((m['username'], m['fullName'], True))
+    return ret
